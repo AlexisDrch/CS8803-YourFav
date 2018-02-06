2aa1314e8d6b2d925c51c510fc041ded4e79b0e4
VanessaSrvs <vani-servais@hotmail.com>
2018-02-06 03:26:30 +0100

    added post request, need to give url and object

--- Android/app/src/main/java/com/yourfav/PictureAdapter.java
+++ Android/app/src/main/java/com/yourfav/PictureAdapter.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,9 +16,17 @@ import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Vanessa on 28.01.2018.
@@ -47,9 +56,13 @@ public class PictureAdapter extends ArrayAdapter<Picture> {
 
                     //get the row the clicked button is in
                     RelativeLayout vwParentRow = (RelativeLayout)v.getParent();
+                    ListView lv=(ListView)vwParentRow.getParent();
+                    int pos =lv.getPositionForView(vwParentRow);
 
                     Button btnChild = (Button)vwParentRow.getChildAt(1);
-                    btnChild.setText("I've been clicked!");
+                    Picture pictureToDisplay = getItem(pos);
+                    addFav(pictureToDisplay);
+                    btnChild.setText(pictureToDisplay.getUrl());
 
                 }
             });
@@ -66,5 +79,39 @@ public class PictureAdapter extends ArrayAdapter<Picture> {
 
 
         return convertView;
+    }
+
+    public void addFav(Picture pic){
+        RequestQueue queue = Volley.newRequestQueue(this.getContext().getApplicationContext());
+        StringRequest postRequest = new StringRequest(Request.Method.POST, Constant.API_URL_POST,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Do something with error response
+                        error.printStackTrace();
+                        //Snackbar.make(mCLayout,"Error",Snackbar.LENGTH_LONG).show();
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("name", "Alif");
+                params.put("domain", "http://itsalif.info");
+
+                return params;
+            }
+        };
+
     }
 }
